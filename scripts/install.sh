d84452d0abfb39e31ce4f0da57561361e3af932e
server <hhhoogenberg@gmail.com>
2026-04-01 12:19:25 +0000

    commit

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-#installeren dotnet sdk
+echo -e "\e[32minstalleren dotnet sdk\e[0m"
 sudo apt update
 sudo apt install -y dotnet-sdk-8.0 git
 
-#naar home directory
+echo -e "\e[32mnaar home directory\e[0m"
 cd /home/server/ || exit
 
-#repo clonen of update
+echo -e "\e[32mrepo clonen of updaten\e[0m"
 if [ -d "/home/server/ITM/.git" ]; then
+	echo -e "\e[32mrepo word geupdate\e[0m"
 	cd /home/server/ITM || exit
 	git pull
 else
+	echo -e "\e[32mrepo wordt gecloned\e[0m"
 	git clone git@github.com:chocoletter/ITM.git
 fi
 
-#naar de goeie directory gaan en hem runnen
+echo -e "\e[32mnaar de goeie directory gaan en hem runnen\e[0m"
 cd /home/server/ITM/frontend/EasyDevOps || exit
 dotnet run --urls "http://192.168.10.131:2000"
